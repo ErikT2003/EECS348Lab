2ecfe51d1ed461c16585c32c54a5571615ad35cb
ErikT2003 <williamtran2803@gmail.com>
2024-03-10 23:27:46 -0500

    commit programming problem 2

---
+++ Lab05_ProgrammingProblem1_SaleReport/Lab05_ProgrammingProblem1_SaleReport/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Lab05_ProgrammingProblem1_SaleReport
+//
+//  Created by William Tran on 3/10/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
